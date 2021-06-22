29063d6b4d460bcf58d16568372673edf28a68cf
ZyaireSchool <ziming.qu22@ycdsbk12.ca>
2021-06-22 11:08:30 -0400

    qwqcommit

--- src/CPT/Main.java
+++ src/CPT/Main.java
@@ -172,37 +172,4 @@ public class Main extends Application {
         br.close();
     }
 
-    public static void lineChart(String country, Stage primaryStage){
-        CategoryAxis xAxis = new CategoryAxis();
-        NumberAxis yAxis = new NumberAxis();
-        LineChart<String, Double> chart = new LineChart(xAxis, yAxis);
-        ArrayList<Pervalence> cData = Methods.listByCountry(country);
-        Pervalence c;
-
-        chart.setTitle(country);
-        xAxis.setLabel("Year");
-        yAxis.setLabel("Population");
-
-        XYChart.Series <String, Long> data = new XYChart.Series <String, Long>();
-        data.setName(country);
-
-        for (int intCount = 0; intCount < cData.size(); intCount ++) {
-            c = cData.get(intCount);
-            data.getData().add(new XYChart.Data <String, Long> (c.getYear(), Long.parseLong(c.getPopulation())));
-        }
-
-        chart.getData().add(data);
-
-        Button back = new Button("Back to Menu");
-        back.setOnAction(e -> primaryStage.setScene(new Scene(mainMenu(primaryStage), 300, 250)));
-
-        Button Settings = new Button("Settings");
-        Settings.setOnAction(e -> primaryStage.setScene(new Scene(lineSettings(primaryStage), 300, 250)));
-
-        VBox box = new VBox();
-        box.getChildren().addAll(chart, Settings, back);
-
-        return box;
-    }
-
 }
